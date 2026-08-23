42e97cbe5d5cd0f5b30839280b0f66a0ac245d0f
Hussam Aldossary <222110518@psu.edu.sa>
2026-08-23 15:03:28 +0300

    implementation of ex2

--- lab02/src/main/java/maven/lab02/App.java
+++ lab02/src/main/java/maven/lab02/App.java
@@ -6,8 +6,17 @@ package maven.lab02;
  */
 public class App {
 	public static void main(String[] args) {
+		// implementation of ex1
 		String[] myArray = { "Dr. Skander Turki", "please give me A+", "Hussam Aldossary", "error 404" };
 		PrintableList<String> mylist = new PrintableList<String>(myArray);
 		mylist.printAll();
+		// implementation of ex2
+		NumberBox<Integer> firstNum = new NumberBox<Integer>(30);
+		NumberBox<Integer> secondNum = new NumberBox<Integer>(50);
+		NumberBox<Double> thirdNum = new NumberBox<Double>(100.0);
+		NumberBox<Double> fourthNum = new NumberBox<Double>(-20.0);
+		System.out.println(firstNum.add(secondNum));
+		System.out.println(thirdNum.add(fourthNum));
+
 	}
 }
