e166d5f51ff8325ba5b1f920830cf86a716f4d80
Hussam Aldossary <222110518@psu.edu.sa>
2026-08-23 13:07:20 +0300

    added class PrintableList (param constructor + printAll())

---
+++ lab02/src/main/java/maven/lab02/PrintableList.java
@@ -0,0 +1,18 @@
+package maven.lab02;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class PrintableList<T> {
+	private List<T> mylist;
+
+	public PrintableList(T[] mylist) {
+		this.mylist = Arrays.asList(mylist);
+	}
+
+	public void printAll() {
+		for (T item : this.mylist) {
+			System.out.println(item);
+		}
+	}
+}
