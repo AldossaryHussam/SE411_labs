875abbdf8d1582244b0402f30a52908621a38779
Hussam Aldossary <222110518@psu.edu.sa>
2026-08-23 13:18:38 +0300

    implementing ex1 -calling printAll method-

--- lab02/src/main/java/maven/lab02/App.java
+++ lab02/src/main/java/maven/lab02/App.java
@@ -4,10 +4,10 @@ package maven.lab02;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		String[] myArray = { "Dr. Skander Turki", "please give me A+", "Hussam Aldossary", "error 404" };
+		PrintableList<String> mylist = new PrintableList<String>(myArray);
+		mylist.printAll();
+	}
 }
