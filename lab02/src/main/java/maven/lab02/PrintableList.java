ad7161c324607c44aac9437021d6ae8af286bdec
Hussam Aldossary <222110518@psu.edu.sa>
2026-08-26 10:31:18 +0300

    changing to rest param

--- lab02/src/main/java/maven/lab02/PrintableList.java
+++ lab02/src/main/java/maven/lab02/PrintableList.java
@@ -6,13 +6,13 @@ import java.util.List;
 public class PrintableList<T> {
 	private List<T> mylist;
 
-	public PrintableList(T[] mylist) {
+	public PrintableList(T... mylist) {
 		this.mylist = Arrays.asList(mylist);
 	}
 
 	public void printAll() {
 		for (T item : this.mylist) {
-			System.out.println(item);
+			System.out.printf("%s ", item);
 		}
 	}
 }
