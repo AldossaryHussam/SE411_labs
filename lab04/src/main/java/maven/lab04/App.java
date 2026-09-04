97141cabcc628585b6177a48138d7f672d4010d5
Hussam Aldossary <222110518@psu.edu.sa>
2026-09-05 01:48:16 +0300

    making a basic javafx app+ running it

--- lab04/src/main/java/maven/lab04/App.java
+++ lab04/src/main/java/maven/lab04/App.java
@@ -1,13 +1,27 @@
 package maven.lab04;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App extends Application {
+	public static void main(String[] args) {
+		launch();
+	}
+
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			primaryStage.setTitle("My Project");
+
+			primaryStage.show();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 }
