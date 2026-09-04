869590e26af8522cd7190bc18d128dd73dc058e4
Hussam Aldossary <222110518@psu.edu.sa>
2026-09-04 21:27:15 +0300

    adding lab03+writing unit tests/running maven test+ eclipse configurations

---
+++ Lab03/src/test/java/StackTest.java
@@ -0,0 +1,39 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.NoSuchElementException;
+
+import org.junit.jupiter.api.Test;
+
+import edu.psu.se411.model.Stack;
+
+public class StackTest {
+	Stack<String> stack = new Stack<>();
+
+	@Test
+	public void testPushAndPop() {
+		stack.push("A");
+		stack.push("Z");
+		// successful test case
+
+		// assertEquals("Z", stack.pop());
+
+		// Fail test case
+
+		assertEquals("A", stack.pop());
+
+	}
+
+	@Test
+	public void pop_empty_stack() {
+		Stack<String> stringStack = new Stack<>();
+		NoSuchElementException thrown = assertThrows(NoSuchElementException.class, () -> stringStack.pop(),
+				"Expected pop from empty Stack to throw, but it didn't");
+		// this message was written in a way that was not identical to the message in
+		// the Stack class, so this test will fail
+		assertTrue(thrown.getMessage().equals("Stack is empty, can’t pop"));
+
+	}
+
+}
