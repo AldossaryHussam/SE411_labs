ba9f041896cb7d75c8d505594433b3f1d80edf61
Hussam Aldossary <222110518@psu.edu.sa>
2026-09-04 19:40:52 +0300

    adding class PipLine (complete)

--- lab02/src/main/java/maven/lab02/PipeLine.java
+++ lab02/src/main/java/maven/lab02/PipeLine.java
@@ -1,5 +1,33 @@
-package maven.lab02;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PipeLine<T, R> {
+	private final List<Transformer<?, ?>> transformers;
 
+	// Private constructor
+	private PipeLine(List<Transformer<?, ?>> transformers) {
+		this.transformers = new ArrayList<>(transformers);
+	}
+
+	// Start a new pipeline
+	public static <T> PipeLine<T, T> start() {
+		return new PipeLine<>(new ArrayList<>());
+	}
+
+	// Add a transformer and return new pipeline
+	public <V> PipeLine<T, V> andThen(Transformer<R, V> transformer) {
+		List<Transformer<?, ?>> newTransformers = new ArrayList<>(this.transformers);
+		newTransformers.add(transformer);
+		return new PipeLine<>(newTransformers);
+	}
+
+	// Execute all transformations
+	@SuppressWarnings("unchecked")
+	public R execute(T input) {
+		Object current = input;
+		for (Transformer<?, ?> transformer : transformers) {
+			current = ((Transformer<Object, Object>) transformer).transform(current);
+		}
+		return (R) current;
+	}
 }
