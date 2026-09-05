8db310d310da2a1ec471b9ad8a2bce1ff115d829
Hussam Aldossary <222110518@psu.edu.sa>
2026-09-05 12:46:33 +0300

    add a custom exception

---
+++ lab05/src/main/java/edu/psu/se411/exceptions/InvalidAgeException.java
@@ -0,0 +1,11 @@
+package edu.psu.se411.exceptions;
+
+public class InvalidAgeException extends Exception {
+	public InvalidAgeException() {
+		super("Invalid age provided.");
+	}
+
+	public InvalidAgeException(String message) {
+		super(message);
+	}
+}
