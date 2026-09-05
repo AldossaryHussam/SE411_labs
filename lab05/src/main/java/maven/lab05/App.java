9790b8f97388a688a6d3c1b0113adf24bcb24ffe
Hussam Aldossary <222110518@psu.edu.sa>
2026-09-05 13:12:13 +0300

    lab05 up to ex2

--- lab05/src/main/java/maven/lab05/App.java
+++ lab05/src/main/java/maven/lab05/App.java
@@ -3,21 +3,34 @@ package maven.lab05;
 import java.io.FileReader;
 import java.io.IOException;
 
+import edu.psu.se411.exceptions.InvalidAgeException;
+
 /**
  * Hello world!
  *
  */
 
 public class App {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InvalidAgeException {
 		try {
 			FileReader reader = new FileReader("panda.txt");
 			reader.close();
+
 		} catch (IOException e) {
 
 			System.out.println("File not found: " + e.getMessage());
-			e.printStackTrace();
+			// e.printStackTrace();
 
 		}
+		validateAge(18);
+		validateAge(17);
+
+	}
+
+	public static void validateAge(int age) throws InvalidAgeException {
+		if (age < 18) {
+			throw new InvalidAgeException("Invalid age: " + age);
+		}
+		System.out.println("Valid age: " + age);
 	}
 }
